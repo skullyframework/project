0defbbfa745cfd1e095f57aa5e30763ad291b514
Jay Teguh <jay@tgitriodesign.com>
2014-05-15 20:45:36 +0700

    js setup for packer tool

--- public/default/resources/foundation/config.rb
+++ public/default/resources/foundation/config.rb
@@ -6,7 +6,7 @@ http_path = "/"
 css_dir = "../css"
 sass_dir = "scss"
 images_dir = "../images"
-javascripts_dir = "../js"
+javascripts_dir = "../js_unpacked"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
