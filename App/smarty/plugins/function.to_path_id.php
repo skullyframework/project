376669c613bb679ecd67d86224a50e0cd8658a1e
Jay Teguh <jay@tgitriodesign.com>
2014-05-18 09:35:38 +0700

    removed smarty plugins

--- App/smarty/plugins/function.to_path_id.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function smarty_function_to_path_id($params = array(), &$smarty) {
-    $pathId = \App\Helpers\UrlHelper::toPathId($params['name'], $params['id']);
-    return $pathId;
-}
--- App/smarty/plugins/modifier.translate.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function smarty_modifier_translate($value, $lang) {
-    $string = \App\Helpers\TextHelper::modelTranslator($value, $lang);
-    return $string;
-}
