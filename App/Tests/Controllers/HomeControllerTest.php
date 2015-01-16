74bfd6586fb0a643bd30afd4737695e460c70622
jaycode <teguhwpurwanto@gmail.com>
2015-01-17 05:55:28 +0700

    updated skully

--- App/Tests/Controllers/HomeControllerTest.php
+++ App/Tests/Controllers/HomeControllerTest.php
@@ -3,6 +3,8 @@
 
 namespace Tests\Controllers;
 
+use Skully\App\Helpers\FileHelper;
+
 require_once 'ControllerTest.php';
 
 class HomeControllerTest extends ControllerTest {
@@ -10,13 +12,13 @@ class HomeControllerTest extends ControllerTest {
     public function testSmartyTemplateDir()
     {
         $r = $this->app->getTemplateEngine()->getTemplateDir();
-        $this->assertEquals($this->app->config('basePath').'public/default/App/views/', $r['main']);
+        $this->assertEquals(FileHelper::replaceSeparators($this->app->config('basePath').'public/default/App/views/'), $r['main']);
     }
 
     public function testSmartyPluginsDir()
     {
         $r = $this->app->getTemplateEngine()->getPluginsDir();
-        $this->assertEquals($this->app->config('basePath').'App/smarty/plugins/', $r[0]);
+        $this->assertEquals(FileHelper::replaceSeparators($this->app->config('basePath').'App/smarty/plugins/'), $r[0]);
     }
     public function testIndex()
     {
